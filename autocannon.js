21471598bc3792a6f9aaef003999549bbc60ee2f
Danil D <d.danil.s.96@gmail.com>
2019-12-09 17:55:40 +0300

    Add auto

--- autocannon.js
+++ autocannon.js
@@ -1,19 +1,43 @@
 'use strict'
  
 const autocannon = require('autocannon');
+const title = 'Autocannon for: ' + (process.argv[2] || 'Server');
+
+const config = {
+  connections: [100, 300]
+}
+
+console.log(title);
 
 (async () => {
-  const result = await autocannon({
-    url: 'http://localhost:3000',
-    connections: 100, //default
-    pipelining: 1, // default
-    duration: 10, // default
-  });
-
-  const { requests: { average: avgReq } } = result;
-  const { latency: { average: avgLat } } = result; 
+  const overall = [];
+  for (const conn of config.connections) {
+    const result = await autocannon({
+      url: 'http://localhost:3000',
+      connections: conn, 
+      duration: 5,
+      title: title
+    });
   
-  console.log('Requst', avgReq);
-  console.log('Latency', avgLat);
-  console.log(result);
+    const { requests: { average: avgReq } } = result;
+    const { requests: { p50: medianReq } } = result;
+    const { latency: { average: avgLat } } = result; 
+    const { latency: {  p50: medianLat } } = result;
+    
+    const duration = result.duration;
+    const { requests: { total: reqs } } = result;
+
+    overall.push({
+      'Connections': conn,
+      'Request avg': avgReq,
+      'Request median': medianReq,
+      'Latency avg': avgLat,
+      'Latency median': medianLat,
+      'Latency median': medianLat,
+      'Total requests': reqs,
+      'Duration': duration
+    });
+  }
+
+  console.table(overall);
 })();
